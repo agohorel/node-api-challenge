3fae428c28934ccae32f966a35e5e230794fc029
agohorel <gohorelalex@gmail.com>
2020-02-14 11:38:12 -0500

    add validateProjectID middleware and PUT route for /api/projects

--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -22,6 +22,19 @@ router.post("/", validateProject, async (req, res) => {
   }
 });
 
+router.put("/:id", validateProjectID, validateProject, async (req, res) => {
+  const {
+    body,
+    params: { id }
+  } = req;
+  try {
+    const updatedPost = await projects.update(id, body);
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    res.status(500).json({ error: "Server error :(" });
+  }
+});
+
 function validateProject(req, res, next) {
   const { body } = req;
 
@@ -31,6 +44,20 @@ function validateProject(req, res, next) {
     });
   } else {
     next();
+  }
+}
+
+async function validateProjectID(req, res, next) {
+  const { id } = req.params;
+  try {
+    const project = await projects.get(id);
+    if (!project) {
+      res.status(404).json({ message: "Invalid project ID." });
+    } else {
+      next();
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Server error :(" });
   }
 }
 
