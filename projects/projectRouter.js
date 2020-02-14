497edeaa31844151136f27e696c7fb485e2404a1
agohorel <gohorelalex@gmail.com>
2020-02-14 11:31:47 -0500

    add validatePost middleware and GET/POST routes for /api/projects

--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -3,8 +3,35 @@ const projects = require("../data/helpers/projectModel.js");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("hello from projects router");
+router.get("/", async (req, res) => {
+  try {
+    const allProjects = await projects.get();
+    res.status(200).json(allProjects);
+  } catch (error) {
+    res.status(500).json({ error: "Server error :(" });
+  }
 });
+
+router.post("/", validateProject, async (req, res) => {
+  const { body } = req;
+  try {
+    const newProject = await projects.insert(body);
+    res.status(201).json(newProject);
+  } catch (error) {
+    res.status(500).json({ error: "Server error :(" });
+  }
+});
+
+function validateProject(req, res, next) {
+  const { body } = req;
+
+  if (!body.name || !body.description) {
+    res.status(400).json({
+      message: "Please provide a name and description for the project"
+    });
+  } else {
+    next();
+  }
+}
 
 module.exports = router;
