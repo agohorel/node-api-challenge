b46d45935074df108281a1afb39f1cca1ce4e412
agohorel <gohorelalex@gmail.com>
2020-02-14 12:01:19 -0500

    add validateActionID middleware and PUT route for /api/actions

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -13,7 +13,7 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.post("/:id", validateProjectID, validateProject, async (req, res) => {
+router.post("/:id", validateProjectID, validateAction, async (req, res) => {
   const {
     body,
     params: { id }
@@ -22,6 +22,21 @@ router.post("/:id", validateProjectID, validateProject, async (req, res) => {
     const payload = { ...body, project_id: id };
     const newAction = await actions.insert(payload);
     res.status(201).json(newAction);
+  } catch (error) {
+    res.status(500).json({ error: "Server error :(" });
+  }
+});
+
+router.put("/:id", validateActionID, validateAction, async (req, res) => {
+  const {
+    body,
+    params: { id }
+  } = req;
+  try {
+    const action = await actions.get(id);
+    const payload = { ...body, project_id: action.project_id };
+    const updatedAction = await actions.update(id, payload);
+    res.status(200).json(updatedAction);
   } catch (error) {
     res.status(500).json({ error: "Server error :(" });
   }
@@ -41,7 +56,7 @@ async function validateProjectID(req, res, next) {
   }
 }
 
-function validateProject(req, res, next) {
+function validateAction(req, res, next) {
   const { body } = req;
   if (!body.description || !body.notes) {
     res.status(400).json({
@@ -49,6 +64,20 @@ function validateProject(req, res, next) {
     });
   } else {
     next();
+  }
+}
+
+async function validateActionID(req, res, next) {
+  const { id } = req.params;
+  try {
+    const action = await actions.get(id);
+    if (!action) {
+      res.status(404).json({ message: "Invalid action ID." });
+    } else {
+      next();
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Server error :(" });
   }
 }
 
