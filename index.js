83f563e504093589116d6c4948c1e305ef2b0e6b
agohorel <gohorelalex@gmail.com>
2020-02-14 12:36:56 -0500

    add cors middleware

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const server = express();
 const projectRouter = require("./projects/projectRouter.js");
 const actionRouter = require("./actions/actionRouter.js");
 const port = 5000;
 
 server.use(express.json());
+server.use(cors());
 server.use("/api/projects", projectRouter);
 server.use("/api/actions", actionRouter);
 
